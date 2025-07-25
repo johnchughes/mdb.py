def0b21e58ca87cb2048ac45e89eea9039086b5f
John Hughes <j.c.hughes7@gmail.com>
2025-07-25 21:56:40 +0100

    Added write method to builder

--- MDB/MDBuilder.py
+++ MDB/MDBuilder.py
@@ -7,10 +7,15 @@ class MDBuilder:
     def __init__(self):
         self.components: List[MDComponent] = list()
     
-    def AddComponent(self, component:MDComponent):
+    def AddComponent(self, component:MDComponent) -> None:
         self.components.append(component)
 
-    def Build(self):
+    def Build(self) -> str:
         st_arr = [comp.ToString() for comp in self.components]
         str = "".join(st_arr) # type: ignore # TODO: fix this! 
         return str
+    
+    def Write(self, path):
+        markdown_string = self.Build()
+        with open(path, 'w') as f:
+            f.write(markdown_string)
